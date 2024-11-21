a2f4a61d99add165c72d0408d20fdf8125bb03e6
Serhii Hul <owna.sgul@gmail.com>
2024-11-21 11:07:32 +0200

    refactor calc

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+function calc(a, b, op) {
+  if (op === 'add') {
+    return add(a, b)
+  } else if (op === 'sub') {
+    return sub(a, b)
+  } else if (op === 'multi') {
+    return multi(a, b)
+  } else if (op === 'div') {
+    return div(a, b)
+  } else if (op === 'mod') {
+    return mod(a, b)
+  } else if (op === 'exp') {
+    return exp(a, b)
+  } else if (op === 'perc') {
+    return perc(a, b)
+  } else {
+    throw new Error(`Неподдерживаемая операция: ${op}`)
+  }
+}
+
 function add(a, b) {
   return a + b
 }
@@ -29,3 +49,11 @@ function exp(a, b) {
 function perc(a, b) {
   return (a / 100) * b
 }
+
+console.log(calc(10, 5, 'add'))
+console.log(calc(10, 5, 'sub'))
+console.log(calc(10, 5, 'multi'))
+console.log(calc(10, 5, 'div'))
+console.log(calc(10, 5, 'mod'))
+console.log(calc(2, 3, 'exp'))
+console.log(calc(200, 10, 'perc'))
